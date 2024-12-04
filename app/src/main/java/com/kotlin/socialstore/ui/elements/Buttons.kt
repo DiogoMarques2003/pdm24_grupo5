bbd753f0369a3794ddc1a1540c6c0876e1dd45fa
Diogo Duarte <diogoduarte896@gmail.com>
2024-12-04 23:32:27 +0000

    Fixed login and Added Home Screen
    Created home screen
    Linked Home screen navigation with login and register
    Fixed buttons and texts on login

--- app/src/main/java/com/kotlin/socialstore/ui/elements/Buttons.kt
+++ app/src/main/java/com/kotlin/socialstore/ui/elements/Buttons.kt
@@ -3,19 +3,17 @@ package com.kotlin.socialstore.ui.elements
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun buttonElement(text: String, color: Color, onClick: () -> Unit) {
-    Button(modifier = Modifier.size(width = 20.dp, height = 20.dp),
+fun ButtonElement(text: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Button(
+        modifier = modifier.size(50.dp),
         onClick = onClick,
-        colors = ButtonDefaults.buttonColors(color),
         shape = RoundedCornerShape(35)
     ) {
         Text(text = text, fontSize = 15.sp)
--- app/src/main/java/com/kotlin/socialstore/ui/screens/AppNavigation.kt
+++ app/src/main/java/com/kotlin/socialstore/ui/screens/AppNavigation.kt
@@ -3,40 +3,45 @@ package com.kotlin.socialstore.ui.screens
 import DonationItem
 import ForgotPasswordPage
 import SubmitDonationPage
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
+private val defaultPadding = 16.dp
+
 @Composable
 fun AppNavigation(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
+    val modifierCustom: Modifier = modifier.padding(start = defaultPadding, end = defaultPadding)
 
-    NavHost(navController = navController, startDestination = "login_screen") {
+    NavHost(navController = navController, startDestination = "home_screen") {
         composable("login_screen") {
-            LoginPage(navController, modifier)
+            LoginPage(navController, modifierCustom)
         }
         composable("register_screen") {
-            RegisterPage(navController, modifier)
+            RegisterPage(navController, modifierCustom)
         }
         composable("donation_success") {
-            DonationSuccessPage(navController, modifier)
+            DonationSuccessPage(navController, modifierCustom)
         }
         composable("submit_donation") {
             SubmitDonationPage(
                 navController,
-                modifier,
+                modifierCustom,
                 onSubmitDonations = { items ->
-                    
+
                 }
             )
         }
-       composable("forgot_password_screen") {
-            ForgotPasswordPage(navController, modifier)
+        composable("forgot_password_screen") {
+            ForgotPasswordPage(navController, modifierCustom)
         }
         composable("home_screen") {
-            HomePage(navController, modifier)
+            HomePage(navController, modifierCustom)
         }
 
     }
