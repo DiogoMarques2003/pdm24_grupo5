dfdd7a4966e9f36dd6bdb94928f09ed04eec74d7
Diogo Marques <diogomarques2003@protonmail.com>
2024-12-09 19:51:13 +0000

    fix firebase deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.common.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
