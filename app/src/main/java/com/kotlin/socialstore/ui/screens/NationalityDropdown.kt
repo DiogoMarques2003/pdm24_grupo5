bece40951a07a493a65219e04e0cade29330980b
Miguel Simões <lmcms2000@gmail.com>
2024-12-09 15:59:41 +0000

    Register Screen improvment + nationalities dropdown

---
+++ app/src/main/java/com/kotlin/socialstore/ui/screens/NationalityDropdown.kt
@@ -0,0 +1,88 @@
+package com.kotlin.socialstore.ui.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
+import com.kotlin.socialstore.R
+import com.kotlin.socialstore.ui.elements.OutlinedTextfieldElement
+
+@Composable
+fun NationalityDropdown(
+    selectedNationality: String,
+    onNationalitySelected: (String) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    val nationalities = context.resources.getStringArray(R.array.nationalities)
+    val icon = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown
+    var textFieldSize by remember { mutableStateOf(Size.Zero)}
+
+    Box() {
+
+
+        OutlinedTextfieldElement(
+            value = selectedNationality,
+            onValueChange = {},
+            readOnly = true,
+            labelText = stringResource(R.string.nationality_text_field),
+            trailingIcon = {
+                Icon(icon,"contentDescription",
+                    Modifier.clickable { expanded = !expanded }
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    textFieldSize = coordinates.size.toSize()
+                },
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .width(with(LocalDensity.current){ textFieldSize.width.toDp() })
+                .heightIn(max = 400.dp)
+        ) {
+            nationalities.forEach { nationality ->
+                DropdownMenuItem(
+                    text = {
+                        Text(
+                            text = nationality,
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    },
+                    onClick = {
+                        onNationalitySelected(nationality)
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
+}
