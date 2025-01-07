b95ec40e95c723164ccd0885eda89a468fc00c6e
Miguel <lmcms2000@gmail.com>
2025-01-06 19:34:32 -0800

    Changed to Elevated Card

--- app/src/main/java/com/kotlin/socialstore/ui/elements/Lists.kt
+++ app/src/main/java/com/kotlin/socialstore/ui/elements/Lists.kt
@@ -1,12 +1,9 @@
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -22,6 +19,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedCard
@@ -85,15 +83,12 @@ fun <T> RowList(
         modifier = modifier
     ) {
         items(items) { item ->
-            Surface(
+            ElevatedCard(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clickable { onItemClick(item) },
-                shape = RoundedCornerShape(0.dp),
-                border = BorderStroke(
-                    width = 1.dp,
-                    color = MaterialTheme.colorScheme.outline.copy(alpha = 0.12f)
-                ),
+                    .padding(bottom = UiConstants.itemSpacing),
+                shape = RoundedCornerShape(8.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
             ) {
                 Row(
                     modifier = Modifier
